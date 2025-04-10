3888709532699673567822f5ac127e07f03d5a45
李子木 <zimu.li@lofty.com>
2025-04-10 22:02:07 +0800

    feat: 搭建六边形架构

---
+++ parking-billing-bootstrap/src/main/java/pers/zymir/parking/billing/ParkingBillingApplication.java
@@ -0,0 +1,12 @@
+package pers.zymir.parking.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingBillingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingBillingApplication.class);
+    }
+}
+
